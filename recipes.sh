b2145d2c1521cda959ee57cfa3acec44c642f80e
Jianfei Chen <chrisjianfeichen@gmail.com>
2019-12-28 12:44:12 +0800

    Update recipes.sh

--- recipes.sh
+++ recipes.sh
@@ -1,9 +1,9 @@
+# Environment: python 3.6 + (anaconda) pytorch 1.3.1 + nvidia apex
+
+mkdir results/
+./train resnet18 
+./train resnet18 "-c quantize --persample=True"
 # ./test results/resnet50 "-c quantize"
-CUDA_VISIBLE_DEVICES=1,6 ./test_cifar 2 preact_resnet20 200 "-c quantize --persample=True"
-
-./train_cifar preact_resnet20
-./train_cifar preact_resnet56
-./train_cifar preact_resnet110
-./train_cifar preact_resnet164
-./train_cifar preact_resnet1001
-
+./train_cifar preact_resnet56 preact_resnet56
+./train_cifar preact_resnet56 preact_resnet56 "-c quantize --persample=True"
+#:CUDA_VISIBLE_DEVICES=1,6 ./test_cifar 2 preact_resnet20 200 "-c quantize --persample=True"

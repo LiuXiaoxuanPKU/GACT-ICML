4aa2afcdbc41c0a16504c35882c834332ede0fea
Jianfei Chen <chrisjianfeichen@gmail.com>
2020-03-29 10:57:49 +0800

    No DALI

--- image_classification/dataloaders.py
+++ image_classification/dataloaders.py
@@ -5,99 +5,99 @@ import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
 DATA_BACKEND_CHOICES = ['pytorch']
-try:
-    from nvidia.dali.plugin.pytorch import DALIClassificationIterator
-    from nvidia.dali.pipeline import Pipeline
-    import nvidia.dali.ops as ops
-    import nvidia.dali.types as types
-    DATA_BACKEND_CHOICES.append('dali-gpu')
-    DATA_BACKEND_CHOICES.append('dali-cpu')
-except ImportError:
-    print("Please install DALI from https://www.github.com/NVIDIA/DALI to run this example.")
+# try:
+#     from nvidia.dali.plugin.pytorch import DALIClassificationIterator
+#     from nvidia.dali.pipeline import Pipeline
+#     import nvidia.dali.ops as ops
+#     import nvidia.dali.types as types
+#     DATA_BACKEND_CHOICES.append('dali-gpu')
+#     DATA_BACKEND_CHOICES.append('dali-cpu')
+# except ImportError:
+#     print("Please install DALI from https://www.github.com/NVIDIA/DALI to run this example.")
 
 
-class HybridTrainPipe(Pipeline):
-    def __init__(self, batch_size, num_threads, device_id, data_dir, crop, dali_cpu=False):
-        super(HybridTrainPipe, self).__init__(batch_size, num_threads, device_id, seed = 12 + device_id)
-        if torch.distributed.is_initialized():
-            local_rank = torch.distributed.get_rank()
-            world_size = torch.distributed.get_world_size()
-        else:
-            local_rank = 0
-            world_size = 1
-
-        self.input = ops.FileReader(
-                file_root = data_dir,
-                shard_id = local_rank,
-                num_shards = world_size,
-                random_shuffle = True)
-
-        if dali_cpu:
-            dali_device = "cpu"
-            self.decode = ops.HostDecoderRandomCrop(device=dali_device, output_type=types.RGB,
-                                                    random_aspect_ratio=[0.75, 4./3.],
-                                                    random_area=[0.08, 1.0],
-                                                    num_attempts=100)
-        else:
-            dali_device = "gpu"
-            # This padding sets the size of the internal nvJPEG buffers to be able to handle all images from full-sized ImageNet
-            # without additional reallocations
-            self.decode = ops.nvJPEGDecoderRandomCrop(device="mixed", output_type=types.RGB, device_memory_padding=211025920, host_memory_padding=140544512,
-                                                      random_aspect_ratio=[0.75, 4./3.],
-                                                      random_area=[0.08, 1.0],
-                                                      num_attempts=100)
-
-        self.res = ops.Resize(device=dali_device, resize_x=crop, resize_y=crop, interp_type=types.INTERP_TRIANGULAR)
-        self.cmnp = ops.CropMirrorNormalize(device = "gpu",
-                                            output_dtype = types.FLOAT,
-                                            output_layout = types.NCHW,
-                                            crop = (crop, crop),
-                                            image_type = types.RGB,
-                                            mean = [0.485 * 255,0.456 * 255,0.406 * 255],
-                                            std = [0.229 * 255,0.224 * 255,0.225 * 255])
-        self.coin = ops.CoinFlip(probability = 0.5)
-
-    def define_graph(self):
-        rng = self.coin()
-        self.jpegs, self.labels = self.input(name = "Reader")
-        images = self.decode(self.jpegs)
-        images = self.res(images)
-        output = self.cmnp(images.gpu(), mirror = rng)
-        return [output, self.labels]
-
-
-class HybridValPipe(Pipeline):
-    def __init__(self, batch_size, num_threads, device_id, data_dir, crop, size):
-        super(HybridValPipe, self).__init__(batch_size, num_threads, device_id, seed = 12 + device_id)
-        if torch.distributed.is_initialized():
-            local_rank = torch.distributed.get_rank()
-            world_size = torch.distributed.get_world_size()
-        else:
-            local_rank = 0
-            world_size = 1
-
-        self.input = ops.FileReader(
-                file_root = data_dir,
-                shard_id = local_rank,
-                num_shards = world_size,
-                random_shuffle = False)
-
-        self.decode = ops.nvJPEGDecoder(device = "mixed", output_type = types.RGB)
-        self.res = ops.Resize(device = "gpu", resize_shorter = size)
-        self.cmnp = ops.CropMirrorNormalize(device = "gpu",
-                output_dtype = types.FLOAT,
-                output_layout = types.NCHW,
-                crop = (crop, crop),
-                image_type = types.RGB,
-                mean = [0.485 * 255,0.456 * 255,0.406 * 255],
-                std = [0.229 * 255,0.224 * 255,0.225 * 255])
-
-    def define_graph(self):
-        self.jpegs, self.labels = self.input(name = "Reader")
-        images = self.decode(self.jpegs)
-        images = self.res(images)
-        output = self.cmnp(images)
-        return [output, self.labels]
+# class HybridTrainPipe(Pipeline):
+#     def __init__(self, batch_size, num_threads, device_id, data_dir, crop, dali_cpu=False):
+#         super(HybridTrainPipe, self).__init__(batch_size, num_threads, device_id, seed = 12 + device_id)
+#         if torch.distributed.is_initialized():
+#             local_rank = torch.distributed.get_rank()
+#             world_size = torch.distributed.get_world_size()
+#         else:
+#             local_rank = 0
+#             world_size = 1
+#
+#         self.input = ops.FileReader(
+#                 file_root = data_dir,
+#                 shard_id = local_rank,
+#                 num_shards = world_size,
+#                 random_shuffle = True)
+#
+#         if dali_cpu:
+#             dali_device = "cpu"
+#             self.decode = ops.HostDecoderRandomCrop(device=dali_device, output_type=types.RGB,
+#                                                     random_aspect_ratio=[0.75, 4./3.],
+#                                                     random_area=[0.08, 1.0],
+#                                                     num_attempts=100)
+#         else:
+#             dali_device = "gpu"
+#             # This padding sets the size of the internal nvJPEG buffers to be able to handle all images from full-sized ImageNet
+#             # without additional reallocations
+#             self.decode = ops.nvJPEGDecoderRandomCrop(device="mixed", output_type=types.RGB, device_memory_padding=211025920, host_memory_padding=140544512,
+#                                                       random_aspect_ratio=[0.75, 4./3.],
+#                                                       random_area=[0.08, 1.0],
+#                                                       num_attempts=100)
+#
+#         self.res = ops.Resize(device=dali_device, resize_x=crop, resize_y=crop, interp_type=types.INTERP_TRIANGULAR)
+#         self.cmnp = ops.CropMirrorNormalize(device = "gpu",
+#                                             output_dtype = types.FLOAT,
+#                                             output_layout = types.NCHW,
+#                                             crop = (crop, crop),
+#                                             image_type = types.RGB,
+#                                             mean = [0.485 * 255,0.456 * 255,0.406 * 255],
+#                                             std = [0.229 * 255,0.224 * 255,0.225 * 255])
+#         self.coin = ops.CoinFlip(probability = 0.5)
+#
+#     def define_graph(self):
+#         rng = self.coin()
+#         self.jpegs, self.labels = self.input(name = "Reader")
+#         images = self.decode(self.jpegs)
+#         images = self.res(images)
+#         output = self.cmnp(images.gpu(), mirror = rng)
+#         return [output, self.labels]
+#
+#
+# class HybridValPipe(Pipeline):
+#     def __init__(self, batch_size, num_threads, device_id, data_dir, crop, size):
+#         super(HybridValPipe, self).__init__(batch_size, num_threads, device_id, seed = 12 + device_id)
+#         if torch.distributed.is_initialized():
+#             local_rank = torch.distributed.get_rank()
+#             world_size = torch.distributed.get_world_size()
+#         else:
+#             local_rank = 0
+#             world_size = 1
+#
+#         self.input = ops.FileReader(
+#                 file_root = data_dir,
+#                 shard_id = local_rank,
+#                 num_shards = world_size,
+#                 random_shuffle = False)
+#
+#         self.decode = ops.nvJPEGDecoder(device = "mixed", output_type = types.RGB)
+#         self.res = ops.Resize(device = "gpu", resize_shorter = size)
+#         self.cmnp = ops.CropMirrorNormalize(device = "gpu",
+#                 output_dtype = types.FLOAT,
+#                 output_layout = types.NCHW,
+#                 crop = (crop, crop),
+#                 image_type = types.RGB,
+#                 mean = [0.485 * 255,0.456 * 255,0.406 * 255],
+#                 std = [0.229 * 255,0.224 * 255,0.225 * 255])
+#
+#     def define_graph(self):
+#         self.jpegs, self.labels = self.input(name = "Reader")
+#         images = self.decode(self.jpegs)
+#         images = self.res(images)
+#         output = self.cmnp(images)
+#         return [output, self.labels]
 
 
 class DALIWrapper(object):

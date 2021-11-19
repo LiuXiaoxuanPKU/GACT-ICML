899e788919504729b006827db08aa1d8c46412f1
LiuXiaoxuanPKU <lilyliupku@gmail.com>
2021-11-19 07:52:29 +0000

    clean controller, add swap

--- actnn/actnn/controller.py
+++ actnn/actnn/controller.py
@@ -4,101 +4,31 @@ from actnn.ops import op_quantize
 from actnn.ops import op_dequantize
 from actnn.autoprec import AutoPrecision
 
+
 class Controller:
     '''
     default_bit: if auto_prec = False, default_bit is the fix number of quantize bits
                  if auto_prec = True, default_bit is the average number of quantize bits
-    auto_prec: if auto_prec = True, auto precision is turned on. 
+    auto_prec: if auto_prec = True, auto precision is turned on.
     single_quantize: if single_quantize = True, tensors that share the same storage will only be quantized once
     verbose: print debug log
     '''
 
-    def __init__(self, default_bit=4, auto_prec=True, single_quantize=True, verbose=False, use_error=True, swap=False):
+    def __init__(self, default_bit=4, verbose=False, swap=False, debug=False):
         self.unrelated_tensors = set()
         self.verbose = verbose
-        self.single_quantize = single_quantize
-        self.tensor_versions = {}
-        # tensors already quantized and referenced by other tensors
-        # key: tensor_id, value: tensor data
-        self.quantized_tensors = {}
-        # tensors that do not need to be quantized
-        # the quantized results can be looked up in the self.quantized_tensors
-        # key: tensor_id, value: reference_tensor_id
-        self.not_quantize_tensors = {}
-
-        self.tensor_id = 0  # tensor_id starts from 0
-        self.init_iter = True
-        self.errors = []
-        self.all_tensors = {}
-
-        self.auto_prec = auto_prec
-        self.ap = None
         self.default_bit = default_bit
-        self.groups = []
-        self.dims = []
-        self.id2group = {}
-        self.grad_fns = {}
-        self.gcnt = 0
-        self.use_error = use_error
+        self.debug = debug
 
         self.swap = swap
         self.swap_out_stream = torch.cuda.Stream()
         self.swap_in_stream = torch.cuda.Stream()
         self.compute_stream = torch.cuda.current_stream()
-
-        self.iter_cnt = 0
-        self.quantize_layer = -1
-
+        self.ptr_qtensor_map = {}
 
     def filter_tensors(self, pairs):
         for k, v in pairs:
             self.unrelated_tensors.add(v.data_ptr())
-
-    def iterate(self, model):
-        self.iter_cnt += 1
-        # if self.verbose and self.iter_cnt > 2:
-        #     exit(0)
-
-        if self.init_iter and self.auto_prec:
-            dims = torch.tensor(self.dims, dtype=torch.long)
-            group2id = {}
-            group_id = 0
-            for layer_id in self.grad_fns:
-                grad_fn = self.grad_fns[layer_id]
-                if grad_fn not in group2id:
-                    group2id[grad_fn] = group_id
-                    group_id += 1
-
-            groups = []
-            for i in range(self.tensor_id):
-                groups.append(group2id[self.grad_fns[i]])
-
-            if self.auto_prec:
-                self.ap = AutoPrecision(
-                    self.default_bit, groups, dims, warmup_iters=10)
-
-        if (not self.init_iter) and self.auto_prec:
-            grad = []
-            for param in model.parameters():
-                if param.grad is not None:
-                    param_grad = param.grad.detach().ravel()
-                    grad.append(param_grad)
-            grad = torch.cat(grad, 0)
-            if self.use_error:
-                delats = torch.tensor(self.errors)
-            else:
-                delats = 2**(-2.0 * self.ap.bits)
-            gsizes = torch.ones_like(delats)
-            self.ap.iterate(grad, delats, gsizes)
-            print(self.ap.bits)
-
-        if self.init_iter:
-            self.init_iter = False
-
-        self.tensor_id = 0
-        self.errors = []
-        self.quantized_tensors = {}
-        self.all_tensors = {}
 
     def check_quantize(self, input_tensor):
         if input_tensor.dtype != torch.float32:
@@ -109,99 +39,52 @@ class Controller:
             return False
         if input_tensor.data_ptr() in self.unrelated_tensors:
             return False
-        if input_tensor.data_ptr() in self.quantized_tensors:
-            return False
         return True
+
+    def iterate(self):
+        del self.ptr_qtensor_map
+        self.ptr_qtensor_map = {}
 
     def quantize(self, input):
         if not self.check_quantize(input):
             return False, input
 
-        cur_tensor_id = self.tensor_id
+        if self.debug:
+            return True, op_quantize(input, self.default_bit), input.shape
 
-        # if cur_tensor_id != self.quantize_layer:
-        #     return False, input
-
-        if self.init_iter:
-            # use grad_fn to categorize groups
-            # grad_fn indicates the operator of previous layer
-            self.grad_fns[cur_tensor_id] = type(input.grad_fn).__name__
-            dim = input.numel() // input.shape[0]
-            self.dims.append(dim)
-
-            if input.data_ptr() in self.tensor_versions and \
-                    input._version == self.tensor_versions[input.data_ptr()][1]:
-                self.not_quantize_tensors[cur_tensor_id] = \
-                    self.tensor_versions[input.data_ptr()][0]
-            else:
-                self.tensor_versions[input.data_ptr()] = (
-                    cur_tensor_id, input._version)
-       
-        # Get quantize bit
-        if not self.init_iter and self.auto_prec:
-            q_bit = self.ap.bits[cur_tensor_id]
-            q_bit = (q_bit.item() + 1) / 2 * 2
-        else:
-            q_bit = self.default_bit
-
-        if self.use_error or self.verbose:
-            self.all_tensors[cur_tensor_id] = input
-
-        # Get quantized tensor
-        if not self.init_iter and self.single_quantize and cur_tensor_id in self.not_quantize_tensors:
-            if self.verbose:
-                print("Not quantize %d, Reference tensor %d" %
-                          (cur_tensor_id, self.not_quantize_tensors[cur_tensor_id]))
-            q_input = self.quantized_tensors[self.not_quantize_tensors[cur_tensor_id]]
-        else:
-            q_input = op_quantize(input, q_bit)
+        input_shape = input.shape
+        key = (input.data_ptr() + input.sum(), input._version)
+        # ==================start stream===================
+        # check if the tensor is already quantized
+        if key not in self.ptr_qtensor_map:
+            # quantize
+            q_inputs = op_quantize(input, self.default_bit)
             if self.swap:
-                q_input = self.swap_to_cpu(q_input)
+                q_input_cpu = torch.empty(
+                    q_inputs[0].shape, dtype=q_inputs[0].dtype, device='cpu', pin_memory=True)
+                q_input_cpu.copy_(q_inputs[0], non_blocking=True)
+                q_input_gpu = q_inputs[0]
+                del q_input_gpu
+                q_inputs[0] = q_input_cpu
+            self.ptr_qtensor_map[key] = q_inputs
+        # ==================finish stream==================
 
-        if not self.init_iter and self.single_quantize and cur_tensor_id in self.not_quantize_tensors.values():
-            self.quantized_tensors[cur_tensor_id] = q_input
-
-        self.tensor_id += 1
-        return (True, q_input, input.shape, cur_tensor_id)
+        return True, key, input_shape
 
     def dequantize(self, input):
         quantized = input[0]
         if not quantized:
             return input[1]
 
-        _, q_input, input_shape, cur_tensor_id = input
-        
-        if self.swap:
-            q_input = self.swap_to_gpu(q_input)
+        if self.debug:
+            _, q_inputs, input_shape = input
+            ret = op_dequantize(q_inputs, input_shape)
+            return ret
 
-        # if cur_tensor_id != self.quantize_layer:
-        #     return input[1]
+        _, key, input_shape = input
+        q_inputs = self.ptr_qtensor_map[key]
+        if not q_inputs[0].is_cuda:
+            self.ptr_qtensor_map[key][0] = q_inputs[0].cuda()
 
-        r = op_dequantize(q_input, input_shape)
-        if self.verbose or self.use_error:
-            diff_tensor = self.all_tensors[cur_tensor_id] - r
-            diff_ratio = (diff_tensor**2).sum() / \
-                (self.all_tensors[cur_tensor_id]**2).sum()
-            if self.verbose:
-                print("layer = %d, shape %s, diff ratio = %.10f, %s" %
-                      (cur_tensor_id, input_shape, diff_ratio.item(),
-                       type(self.all_tensors[cur_tensor_id].grad_fn).__name__))
-            self.errors.insert(0, (diff_tensor**2).sum())
-        return r
-
-
-    def swap_to_cpu(self, q_input):
-        tensor = q_input[0]
-        # fwd_values[tid] = tensor.to(float).mean()
-        self.swap_out_stream.wait_stream(self.compute_stream)
-        with torch.cuda.stream(self.swap_out_stream):
-            tensor.record_stream(self.swap_out_stream) # tell default stream to keep tensor until swap_stream finishes swapping
-            tensor_cpu = torch.empty(tensor.shape, dtype=tensor.dtype, device='cpu', pin_memory=True)
-            tensor_cpu.copy_(tensor, non_blocking = True)
-        q_input[0] = tensor_cpu
-        return q_input
-
-
-    def swap_to_gpu(self, q_input):
-        q_input[0] = q_input[0].cuda()
-        return q_input
+        ret = op_dequantize(q_inputs, input_shape)
+        return ret
--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,11 +1,12 @@
+from actnn.controller import Controller
+from actnn import get_memory_usage, compute_tensor_bytes
+import time
 import random
 import numpy as np
 import torch
 import torch.nn as nn
 torch.manual_seed(0)
 
-from actnn import get_memory_usage, compute_tensor_bytes
-from actnn.controller import Controller
 
 def set_random_seed(seed):
     random.seed(seed)
@@ -15,37 +16,45 @@ def set_random_seed(seed):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
+
 class TestNet(nn.Module):
     def __init__(self, num_classes: int = 1000, dropout: float = 0.5) -> None:
         super().__init__()
         self.features = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=7, stride=2),
-            # nn.Conv2d(64, 192, kernel_size=5, padding=2),
-            # nn.ReLU(inplace=False),
-            # nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.ReLU(inplace=False),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=False),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=False),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=False),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=False),
+            nn.MaxPool2d(kernel_size=3, stride=2),
         )
         self.classifier = nn.Sequential(
-            nn.Linear(40000, num_classes)
+            nn.Linear(9216, num_classes)
         )
 
     def forward(self, x):
         x = self.features(x)
-        print("pool output shape: ", x.shape)
+        # print("pool output shape: ", x.shape)
         # x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.classifier(x)
         return x
 
 
-N = 128
+N = 256
 x = torch.rand((N, 3, 224, 224), device="cuda")
 num_classes = 100
 y = torch.randint(0, num_classes, (N, num_classes), device="cuda").float()
 
 
-def train(controller = None):
+def train(controller=None):
     set_random_seed(0)
     model = TestNet(num_classes=100).cuda()
     if controller is not None:
@@ -56,10 +65,13 @@ def train(controller = None):
     print("===========Start Train==========")
     losses = []
     mems = []
-    for t in range(2):
+    train_ips_list = []
+    model.train()
+    end = time.time()
+    for t in range(20):
         y_pred = model(x)
         loss = criterion(y_pred, y)
-        if t % 10 == 9:
+        if t % 1 == 0:
             losses.append(loss.item())
             print(t, loss.item())
 
@@ -71,45 +83,73 @@ def train(controller = None):
 
         after_backward_mem = get_memory_usage()
         mems.append(before_backward_mem - after_backward_mem)
+        bs = x.shape[0]
+        batch_total_time = time.time() - end
+        train_ips = bs / batch_total_time
+        train_ips_list.append(train_ips)
+        end = time.time()
         if controller:
-            controller.iterate(model)
-        print("----------------------------------")
-    
-    return losses, mems
+            controller.iterate()
+        # print("----------------------------------")
+
+    return losses, mems, train_ips_list
 
 
 if __name__ == "__main__":
-    # original 
-    orig_losses, org_mems = train()
+    # original warmup
+    orig_losses, org_mems, org_ips_list = train()
+
+    # original
+    orig_losses, org_mems, org_ips_list = train()
+
+    # 4 bit debug
+    debug_controller = Controller(verbose=False, swap=False, debug=True)
+
+    def pack_hook(tensor):
+        return debug_controller.quantize(tensor)
+
+    def unpack_hook(tensor):
+        return debug_controller.dequantize(tensor)
+    with torch.autograd.graph.saved_tensors_hooks(pack_hook, unpack_hook):
+        q4_debug_losses, q4_debug_mems, q4_debug_ips_list = train(
+            debug_controller)
 
     # 4 bit quantization
-    controller = Controller(auto_prec=False, verbose=False, swap=False, single_quantize=True)
+    controller = Controller(verbose=False, swap=False, debug=False)
+
     def pack_hook(tensor):
         return controller.quantize(tensor)
+
     def unpack_hook(tensor):
         return controller.dequantize(tensor)
     with torch.autograd.graph.saved_tensors_hooks(pack_hook, unpack_hook):
-        q4_losses, q4_mems = train(controller)
+        q4_losses, q4_mems, q4_ips_list = train(controller)
 
-    # 4 bit with swap
-    swap_controller = Controller(auto_prec=False, verbose=False, swap=True, single_quantize=True)
+    # # 4 bit with swap
+    swap_controller = Controller(
+        verbose=False, swap=True, debug=False)
+
     def pack_hook(tensor):
         return swap_controller.quantize(tensor)
+
     def unpack_hook(tensor):
         return swap_controller.dequantize(tensor)
     with torch.autograd.graph.saved_tensors_hooks(pack_hook, unpack_hook):
-        q4_swap_losses, q4_swap_mems = train(swap_controller)
+        q4_swap_losses, q4_swap_mems, q4_swap_ips_list = train(swap_controller)
 
     # test swap correctness
+    np.testing.assert_allclose(q4_debug_losses, q4_losses)
     np.testing.assert_allclose(q4_losses, q4_swap_losses)
 
     # test memory
+    print("=========Test Memory============")
     print("original activation mems %d MB" % (org_mems[-1]/1024/1024))
     print("4 bit activation mems %d MB" % (q4_mems[-1]/1024/1024))
     print("4 bit + swap activation mems %d MB" % (q4_swap_mems[-1]/1024/1024))
 
-
-
-    
-
-    
+    # test speed
+    print("=========Test Speed============")
+    print("original speed %f ips" % (np.median(org_ips_list)))
+    print("4 bit debug speed %f ips" % (np.median(q4_debug_ips_list)))
+    print("4 bit speed %f ips" % (np.median(q4_ips_list)))
+    print("4 bit + swap speed %f ips" % (np.median(q4_swap_ips_list)))

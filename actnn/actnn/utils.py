5dfecdfdea2d374277111d40ef08650c6b869609
lily <lilyliupku@gmail.com>
2021-12-10 08:50:24 +0000

    update device for get memory

--- actnn/actnn/utils.py
+++ actnn/actnn/utils.py
@@ -62,8 +62,8 @@ def swap_to_cpu(tensor, tid):
 
 def get_memory_usage(print_info=False):
     """Get accurate gpu memory usage by querying torch runtime"""
-    allocated = torch.cuda.memory_allocated(0)
-    reserved = torch.cuda.memory_reserved(0)
+    allocated = torch.cuda.memory_allocated()
+    reserved = torch.cuda.memory_reserved()
     if print_info:
         print("allocated: %.2f MB" % (allocated / 1024 / 1024), flush=True)
         print("reserved:  %.2f MB" % (reserved / 1024 / 1024), flush=True)
